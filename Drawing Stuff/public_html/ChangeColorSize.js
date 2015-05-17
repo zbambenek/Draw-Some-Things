8b8c8a51ee289763540069a23d03391c52b99f5f
zbambenek <zbambenek@gmail.com>
2015-05-16 23:15:34 -0400

    Adding files
    This is the basic stuff we have so far.

---
+++ Drawing Stuff/public_html/ChangeColorSize.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var color = "#000000";
+var size;
+
+//To change the color of the marker.
+function ChangeColor (){
+    /*if (document.getElementById("cbBlack").value){
+        color = "#000000";
+    } else if(document.getElementById("cbBlue").value){
+        color = "#0000FF";
+    } else if (document.getElementById("cbBrown").value){
+        color = "755600";
+    }
+    */
+   if (document.getElementById("ChoseColor").value = cbBlue){
+       color = "#0000FF";
+   }
+}
+
+//To change the size of the marker
+function ChangeSize(){
+    
+}
+
---
+++ Drawing Stuff/public_html/Drawing.js
@@ -0,0 +1,77 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var canvas, stage;
+var drawingCanvas;
+var oldPt;
+var oldMidPt;
+var title;
+var color;
+var stroke;
+var colors;
+var index;
+
+function init() {
+	canvas = document.getElementById("myCanvas");
+	index = 0;
+	colors = "#000000"
+
+	//check to see if we are running in a browser with touch support
+	stage = new createjs.Stage(canvas);
+	stage.autoClear = false;
+	stage.enableDOMEvents(true);
+
+	createjs.Touch.enable(stage);
+	createjs.Ticker.setFPS(24);
+
+	drawingCanvas = new createjs.Shape();
+
+	stage.addEventListener("stagemousedown", handleMouseDown);
+	stage.addEventListener("stagemouseup", handleMouseUp);
+
+	title = new createjs.Text("Click and Drag to draw", "36px Arial", "#777777");
+	title.x = 300;
+	title.y = 200;
+	stage.addChild(title);
+
+	stage.addChild(drawingCanvas);
+	stage.update();
+}
+
+function stop() {
+}
+
+function handleMouseDown(event) {
+	if (stage.contains(title)) {
+		stage.clear();
+		stage.removeChild(title);
+	}
+	//color = colors[(index++) % colors.length];
+        color = "#000000";
+	//stroke = Math.random() * 30 + 10 | 0;
+	oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+	oldMidPt = oldPt.clone();
+	stage.addEventListener("stagemousemove", handleMouseMove);
+}
+
+function handleMouseMove(event) {
+	var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
+
+	drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+        drawingCanvas.graphics.clear().beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+	oldPt.x = stage.mouseX;
+	oldPt.y = stage.mouseY;
+
+	oldMidPt.x = midPt.x;
+	oldMidPt.y = midPt.y;
+
+	stage.update();
+}
+
+function handleMouseUp(event) {
+	stage.removeEventListener("stagemousemove", handleMouseMove);
+}
